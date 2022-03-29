bb32a2406b5fc9928f525cbb12ba0e84a600a73c
shing <chishing@netvigator.com>
2022-03-29 17:11:24 +0800

    Embeded Mongo DB not started

---
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -0,0 +1,31 @@
+package guru.springframework.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
+@Document
+public class Ingredient {
+
+    public Ingredient() {
+    }
+
+    @Id
+    private String id;
+
+    private String description;
+    private BigDecimal amount;
+    private UnitOfMeasure uom;
+
+    private Recipe recipe;
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+}
