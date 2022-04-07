9874c2c56b7248c3f5ca24978862f6398b3dff11
shing <chishing@netvigator.com>
2022-04-07 21:10:58 +0800

    Remove change for reactive mongo, the change for reactive mongo is move to a new project in git-hub recipePrj-mongo-reactive

--- src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.repositories.reactive;
-
-import guru.springframework.model.UnitOfMeasure;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure,String> {
-}
